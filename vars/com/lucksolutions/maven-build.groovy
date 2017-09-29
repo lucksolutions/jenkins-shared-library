567b2107f4065ffe3cbdaa6ed601f34619078ff6
jasonluck <jluck@meetveracity.com>
2017-09-29 10:20:05 -0400

    Load maven settings

---
+++ vars/com/lucksolutions/maven-build.groovy
@@ -0,0 +1,54 @@
+def call(body) {
+
+        def config = [:]
+        body.resolveStrategy = Closure.DELEGATE_FIRST
+        body.delegate = config
+        body()
+
+        if (config.directory == null) {
+            config.directory = "."
+        }
+
+        node {
+            try {
+                stage ('Setup') {
+                    steps {
+                        dir('${config.directory') {
+                            sh 'mvn clean'
+                            def settings = libraryResource 'com/lucksolutions/maven/settings.xml'
+                            writeFile('target/settings.xml',  settings)
+                        }
+                    }
+                }
+                stage('Build') {
+                    tools {
+                        maven 'Maven'
+                    }     
+                    steps {
+                        dir('${config.directory') {
+                            withCredentials([usernamePassword(credentialsId: 'nexus', usernameVariable: 'DEPLOY_USER', passwordVariable: 'DEPLOY_PASSWORD')]) {
+                                sh 'mvn -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -s target/settings.xml deploy'
+                            }
+                        }
+                    }
+                }
+                stage ('Tests') {
+                    parallel 'static': {
+                        sh "echo 'shell scripts to run static tests...'"
+                    },
+                    'unit': {
+                        sh "echo 'shell scripts to run unit tests...'"
+                    },
+                    'integration': {
+                        sh "echo 'shell scripts to run integration tests...'"
+                    }
+                }
+                stage ('Deploy') {
+                    sh "echo 'deploying to server ${config.serverDomain}...'"
+                }
+            } catch (err) {
+                currentBuild.result = 'FAILED'
+                throw err
+            }
+        }
+    }
