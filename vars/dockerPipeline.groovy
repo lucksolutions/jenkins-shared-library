da27436584ba0565f2cfb2329d9bcb2eb43bc42a
jasonluck <jluck@meetveracity.com>
2017-10-02 13:14:56 -0400

    Create docker pipeline

---
+++ vars/dockerPipeline.groovy
@@ -0,0 +1,40 @@
+def call(body) {
+
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    if (config.directory == null) {
+        config.directory = '.'
+    }
+
+    pipeline {
+        agent any
+
+        triggers {
+            //Check SCM every 5 minutes
+            pollSCM('*/5 * * * *')
+        }
+        
+        stages {
+            stage('Docker Build') {
+                steps {
+                    dir("${config.directory}") {
+                        script {
+                            docker.withServer('tcp://ip-10-247-80-51.us-gov-west-1.compute.internal:2375') {
+                                docker.withRegistry('https://index.docker.io/v1/', 'dockerhub') {
+                                    def image = docker.build('${config.imageName}:${BRANCH_NAME}')
+                                    image.push()
+                                    if (env.BRANCH_NAME == 'development') {
+                                        image.push('latest')
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
