3d894a73f797702c1d827645e5c9ed86bc16edd7
jasonluck <jluck@meetveracity.com>
2017-10-02 13:19:43 -0400

    use double quotes

--- vars/dockerPipeline.groovy
+++ vars/dockerPipeline.groovy
@@ -24,7 +24,7 @@ def call(body) {
                         script {
                             docker.withServer('tcp://ip-10-247-80-51.us-gov-west-1.compute.internal:2375') {
                                 docker.withRegistry('https://index.docker.io/v1/', 'dockerhub') {
-                                    def image = docker.build('${config.imageName}:${BRANCH_NAME}')
+                                    def image = docker.build("${config.imageName}:${BRANCH_NAME}")
                                     image.push()
                                     if (env.BRANCH_NAME == 'development') {
                                         image.push('latest')
