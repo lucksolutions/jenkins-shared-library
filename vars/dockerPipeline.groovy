2def66dcbe9bcdc734efa437c480e95ee778040d
jasonluck <jluck@meetveracity.com>
2017-10-04 12:56:51 -0400

    remove pipeline, use stages instead

--- vars/dockerPipeline.groovy
+++ vars/dockerPipeline.groovy
@@ -9,50 +9,40 @@ def call(body) {
         config.directory = '.'
     }
 
-    pipeline {
-        agent any
-
-        triggers {
-            //Check SCM every 5 minutes
-            pollSCM('*/5 * * * *')
-        }
-        
-        stages {
-            stage('Build Image') {
-                steps {
-                    dir("${config.directory}") {
-                        script {
-                            docker.withServer('tcp://ip-10-247-80-51.us-gov-west-1.compute.internal:2375') {
-                                docker.withRegistry('https://index.docker.io/v1/', 'dockerhub') {
-                                    def image = docker.build("${config.imageName}:${BRANCH_NAME}")
-                                }
-                            }
+    
+    stage('Build Image') {
+        steps {
+            dir("${config.directory}") {
+                script {
+                    docker.withServer('tcp://ip-10-247-80-51.us-gov-west-1.compute.internal:2375') {
+                        docker.withRegistry('https://index.docker.io/v1/', 'dockerhub') {
+                            def image = docker.build("${config.imageName}:${BRANCH_NAME}")
                         }
                     }
                 }
             }
-            stage('Deploy Image') {
-                steps {
-                    sh 'echo "Deploying Docker Container..."'
-                }
-            }
-            stage('Test Image') {
-                steps {
-                    sh 'echo "Executing test cases..."'
-                }
-            }
-            stage('Push to Registry') {
-                steps {
-                    dir("${config.directory}") {
-                        script {
-                            docker.withServer('tcp://ip-10-247-80-51.us-gov-west-1.compute.internal:2375') {
-                                docker.withRegistry('https://index.docker.io/v1/', 'dockerhub') {
-                                    def image = docker.build("${config.imageName}:${BRANCH_NAME}")
-                                    image.push()
-                                    if (env.BRANCH_NAME == 'development') {
-                                        image.push('latest')
-                                    }
-                                }
+        }
+    }
+    stage('Deploy Image') {
+        steps {
+            sh 'echo "Deploying Docker Container..."'
+        }
+    }
+    stage('Test Image') {
+        steps {
+            sh 'echo "Executing test cases..."'
+        }
+    }
+    stage('Push to Registry') {
+        steps {
+            dir("${config.directory}") {
+                script {
+                    docker.withServer('tcp://ip-10-247-80-51.us-gov-west-1.compute.internal:2375') {
+                        docker.withRegistry('https://index.docker.io/v1/', 'dockerhub') {
+                            def image = docker.build("${config.imageName}:${BRANCH_NAME}")
+                            image.push()
+                            if (env.BRANCH_NAME == 'development') {
+                                image.push('latest')
                             }
                         }
                     }
--- vars/mavenPipeline.groovy
+++ vars/mavenPipeline.groovy
@@ -9,32 +9,21 @@ def call(body) {
         config.directory = '.'
     }
 
-    pipeline {
-        agent any
-
-        triggers {
-            //Check SCM every 5 minutes
-            pollSCM('*/5 * * * *')
-        }
-
+    stage('Maven Build') {
         tools {
             maven 'Maven'
         }
-        
-        stages {
-            stage('Maven Build') {
-                steps {
-                    dir("${config.directory}") {
-                        script {
-                            def mavenSettings = libraryResource 'com/lucksolutions/maven/settings.xml'
-                            writeFile file: 'settings.xml', text: mavenSettings
-                        }
-                        withCredentials([usernamePassword(credentialsId: 'nexus', usernameVariable: 'DEPLOY_USER', passwordVariable: 'DEPLOY_PASSWORD')]) {
-                            sh 'mvn -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -s settings.xml clean deploy'
-                        }
-                    }
+        steps {
+            dir("${config.directory}") {
+                script {
+                    def mavenSettings = libraryResource 'com/lucksolutions/maven/settings.xml'
+                    writeFile file: 'settings.xml', text: mavenSettings
+                }
+                withCredentials([usernamePassword(credentialsId: 'nexus', usernameVariable: 'DEPLOY_USER', passwordVariable: 'DEPLOY_PASSWORD')]) {
+                    sh 'mvn -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -s settings.xml clean deploy'
                 }
             }
         }
     }
+
 }
