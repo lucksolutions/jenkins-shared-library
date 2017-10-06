09a457821b7b35e01f924f720f78f536c4dfdc58
jasonluck <jluck@meetveracity.com>
2017-10-06 15:32:47 -0400

    use build_url

--- vars/notifyBuild.groovy
+++ vars/notifyBuild.groovy
@@ -7,7 +7,7 @@ def call(String buildStatus = 'STARTED') {
   def colorName = 'RED'
   def colorCode = '#FF0000'
   def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
-  def summary = "${subject} (${BLUE_OCEAN_URL})"
+  def summary = "${subject} (${env.BUILD_URL})"
   def details = """<p>STARTED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
     <p>Check console output at "<a href="${env.BUILD_URL}">${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>"</p>"""
  
