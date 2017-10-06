7b915c86c1c5664fef5d97f5d8027258576c8d46
jasonluck <jluck@meetveracity.com>
2017-10-06 17:27:13 -0400

    try to mark as unstable when tests fail

--- vars/mavenPipeline.groovy
+++ vars/mavenPipeline.groovy
@@ -33,10 +33,7 @@ def call(body) {
 
             stage('Unit Testing') {
                 try {
-                    sh 'mvn -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Ddockerfile.skip=true -s settings.xml test'
-                } catch (e) {
-                    currentBuild.result = "UNSTABLE"
-                    throw e
+                    sh 'mvn -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Ddockerfile.skip=true -Dmaven.test.failure.ignore=true -s settings.xml test'
                 } finally {
                     echo 'Publishing Test Reports...'
                     step([$class: 'JUnitResultArchiver', testResults: '**/surefire-reports/*.xml', healthScaleFactor: 1.0, allowEmptyResults: true])
