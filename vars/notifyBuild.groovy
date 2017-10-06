224c8698629be4877cfa5305364672f9466432e2
jasonluck <jluck@meetveracity.com>
2017-10-06 17:52:28 -0400

    Use yellow for unstable builds

--- vars/notifyBuild.groovy
+++ vars/notifyBuild.groovy
@@ -14,11 +14,14 @@ def call(String buildStatus = 'STARTED') {
  
   // Override default values based on build status
   if (buildStatus == 'STARTED') {
-    color = 'YELLOW'
-    colorCode = '#FFFF00'
+    color = 'BLUE'
+    colorCode = '#0000FF'
   } else if (buildStatus == 'SUCCESSFUL') {
     color = 'GREEN'
     colorCode = '#00FF00'
+  } else if (buildStatus == 'UNSTABLE') {
+    color = 'YELLOW'
+    colorCode = '#FFFF00'
   } else {
     color = 'RED'
     colorCode = '#FF0000'
