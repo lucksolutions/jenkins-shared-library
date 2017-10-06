c7510d520368d12b133095ad1239eb5bfd3e0e2a
jasonluck <jluck@meetveracity.com>
2017-10-06 10:19:50 -0400

    Add notifications

--- vars/mavenPipeline.groovy
+++ vars/mavenPipeline.groovy
@@ -16,31 +16,41 @@ def call(body) {
             ])
         ])
 
-        stage('Checkout SCM') {
-            checkout scm
-        }
-        stage('Maven Build') {
-            dir("${config.directory}") {
-                try {
-                    def mavenSettings = libraryResource 'com/lucksolutions/maven/settings.xml'
-                    writeFile file: 'settings.xml', text: mavenSettings
-                    withCredentials([usernamePassword(credentialsId: 'nexus', usernameVariable: 'DEPLOY_USER', passwordVariable: 'DEPLOY_PASSWORD')]) {
-                        sh 'mvn -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Ddockerfile.skip=true -s settings.xml clean deploy'
+        try {
+
+            stage('Checkout SCM') {
+                checkout scm
+            }
+            stage('Maven Build') {
+                dir("${config.directory}") {
+                    try {
+                        def mavenSettings = libraryResource 'com/lucksolutions/maven/settings.xml'
+                        writeFile file: 'settings.xml', text: mavenSettings
+                        withCredentials([usernamePassword(credentialsId: 'nexus', usernameVariable: 'DEPLOY_USER', passwordVariable: 'DEPLOY_PASSWORD')]) {
+                            sh 'mvn -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Ddockerfile.skip=true -s settings.xml clean deploy'
+                        }
+                    }
+                    finally {
+                        echo 'Publishing Test Reports...'
+                        step([$class: 'JUnitResultArchiver', testResults: '**/surefire-reports/*.xml', healthScaleFactor: 1.0, allowEmptyResults: true])
+                        publishHTML (target: [
+                            allowMissing: true,
+                            alwaysLinkToLastBuild: false,
+                            keepAll: true,
+                            reportDir: 'target/site/jacoco',
+                            reportFiles: 'index.html',
+                            reportName: "Code Coverage"
+                        ])
                     }
                 }
-                finally {
-                    echo 'Publishing Test Reports...'
-                    step([$class: 'JUnitResultArchiver', testResults: '**/surefire-reports/*.xml', healthScaleFactor: 1.0, allowEmptyResults: true])
-                    publishHTML (target: [
-                        allowMissing: true,
-                        alwaysLinkToLastBuild: false,
-                        keepAll: true,
-                        reportDir: 'target/site/jacoco',
-                        reportFiles: 'index.html',
-                        reportName: "Code Coverage"
-                    ])
-                }
             }
+        } catch (e) {
+            // If there was an exception thrown, the build failed
+            currentBuild.result = "FAILED"
+            throw e
+        } finally {
+            // Success or failure, always send notifications
+            notifyBuild(currentBuild.result)
         }
     }
 
