326b6b019bb6aca40446350b5b620caeb1cd8f22
jasonluck <jluck@meetveracity.com>
2017-10-09 15:55:40 -0400

    turn on debug

--- vars/packerBuild.groovy
+++ vars/packerBuild.groovy
@@ -20,7 +20,7 @@ def call(body) {
                     variables = "${variables} -var ${key}=${config.vars[key]} "
                 }
 
-                sh "packer build ${variables} ${config.packerFile}"
+                sh "packer build -debug ${variables} ${config.packerFile}"
             }
         }
     }
