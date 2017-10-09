98e80789b5d338d36d06f42afdca89d5951869f0
jasonluck <jluck@meetveracity.com>
2017-10-09 10:32:02 -0400

    move dir to node level

--- vars/mavenPipeline.groovy
+++ vars/mavenPipeline.groovy
@@ -17,48 +17,48 @@ def call(body) {
         ])
 
         try {
+            dir("${config.directory}") {
 
-            stage('Checkout SCM') {
-                checkout scm
-            }
-            stage('Maven Build') {
-                dir("${config.directory}") {
+                stage('Checkout SCM') {
+                    checkout scm
+                }
+                stage('Maven Build') {
                     def mavenSettings = libraryResource 'com/lucksolutions/maven/settings.xml'
                     writeFile file: 'settings.xml', text: mavenSettings
                     withCredentials([usernamePassword(credentialsId: 'nexus', usernameVariable: 'DEPLOY_USER', passwordVariable: 'DEPLOY_PASSWORD')]) {
                         sh 'mvn -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Ddockerfile.skip=true -s settings.xml clean compile test-compile'
                     }
                 }
-            }
 
-            try {
-                stage('Unit Testing') {
-                    sh 'mvn -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Ddockerfile.skip=true -Dmaven.test.failure.ignore=true -s settings.xml test'   
-                }
-            } finally {
-                step([$class: 'JUnitResultArchiver', testResults: '**/surefire-reports/*.xml', healthScaleFactor: 1.0, allowEmptyResults: true])
-                if (currentBuild.result == 'UNSTABLE') {
-                    return 
-                }
-            }
-            stage('Package') {
                 try {
-                    sh 'mvn -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Ddockerfile.skip=true -s settings.xml package'
+                    stage('Unit Testing') {
+                        sh 'mvn -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Ddockerfile.skip=true -Dmaven.test.failure.ignore=true -s settings.xml test'   
+                    }
                 } finally {
-                    publishHTML (target: [
-                        allowMissing: true,
-                        alwaysLinkToLastBuild: false,
-                        keepAll: true,
-                        reportDir: 'target/site/jacoco',
-                        reportFiles: 'index.html',
-                        reportName: "Code Coverage"
-                    ])
+                    step([$class: 'JUnitResultArchiver', testResults: '**/surefire-reports/*.xml', healthScaleFactor: 1.0, allowEmptyResults: true])
+                    if (currentBuild.result == 'UNSTABLE') {
+                        return 
+                    }
                 }
-            }
+                stage('Package') {
+                    try {
+                        sh 'mvn -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Ddockerfile.skip=true -s settings.xml package'
+                    } finally {
+                        publishHTML (target: [
+                            allowMissing: true,
+                            alwaysLinkToLastBuild: false,
+                            keepAll: true,
+                            reportDir: 'target/site/jacoco',
+                            reportFiles: 'index.html',
+                            reportName: "Code Coverage"
+                        ])
+                    }
+                }
 
-            stage('Deploy to Repository') {
-                withCredentials([usernamePassword(credentialsId: 'nexus', usernameVariable: 'DEPLOY_USER', passwordVariable: 'DEPLOY_PASSWORD')]) {
-                    sh 'mvn -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Ddockerfile.skip=true -s settings.xml deploy'
+                stage('Deploy to Repository') {
+                    withCredentials([usernamePassword(credentialsId: 'nexus', usernameVariable: 'DEPLOY_USER', passwordVariable: 'DEPLOY_PASSWORD')]) {
+                        sh 'mvn -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Ddockerfile.skip=true -s settings.xml deploy'
+                    }
                 }
             }
         } finally {
