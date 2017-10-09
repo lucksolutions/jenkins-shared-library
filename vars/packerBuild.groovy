73016d4c4688fa402211cd408ba58ffe606e8008
jasonluck <jluck@meetveracity.com>
2017-10-09 13:41:21 -0400

    Adding packer pipeline

---
+++ vars/packerBuild.groovy
@@ -0,0 +1,26 @@
+def call(body) {
+
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    if (config.directory == null) {
+        config.directory = '.'
+    }
+
+    dir("${config.directory}") {
+        withCredentials([amazonWebServices(credentialsId: 'aws', accessKeyVariable: 'AWS_ACCESS_KEY', secretKeyVariable: 'AWS_SECRET_KEY')]) {
+            stage("Build") {
+
+                def variables = "-var aws_access_key=${AWS_ACCESS_KEY} -var aws_secret_key=${AWS_SECRET_KEY} "
+                for (int i=0; i<config.vars.size(); ++i) {
+                    def var = config.vars[i]
+                    variables = variables + "-var ${var} "
+                }
+
+                sh "packer build ${variables} ${config.packerFile}"
+            }
+        }
+    }
+}
---
+++ vars/packerPipeline.groovy
@@ -0,0 +1,35 @@
+def call(body) {
+
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    if (config.directory == null) {
+        config.directory = '.'
+    }
+
+    node {
+        properties([
+            pipelineTriggers([
+                pollSCM('*/5 * * * *')
+            ])
+        ])
+
+        try {
+            stage('Checkout SCM') {
+                checkout scm
+            }
+
+            packerBuild {
+                directory = "${config.directory}"
+                vars = "${config.vars}"
+                packerFile = "${config.packerFile}"
+            }
+        } finally {
+            //Send build notifications if needed
+            notifyBuild(currentBuild.result)
+        }
+    }
+
+}
