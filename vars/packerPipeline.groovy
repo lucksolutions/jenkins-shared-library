5338341c1a1eb0e4791641206fb5af587d50093d
jasonluck <jluck@meetveracity.com>
2017-10-11 08:55:56 -0400

    mark build successful if aborted because of branch indexing.

--- vars/packerPipeline.groovy
+++ vars/packerPipeline.groovy
@@ -17,23 +17,16 @@ def call(body) {
             disableConcurrentBuilds()
         ])
 
-        // if (env.BUILD_CAUSE == 'Branch indexing') {
-        //     currentBuild.result = 'SUCCESSFUL'
-        //     return
-        // }
-
         try {
             stage('Checkout SCM') {
                 def scmVars = checkout scm
-            }
-
-            stage('Check Cause') {
+                
                 //Skip the build if this was caused by branch indexing
                 def causes = currentBuild.rawBuild.getCauses()
                 for (int i=0; i<causes.size(); ++i) {
                     if (causes[i].getShortDescription().contains('Branch indexing')) {
                         //Don't build since this was just branch indexing
-                        currentBuild.result = 'ABORTED'
+                        currentBuild.result = 'SUCCESSFUL'
                         error('Stopping since we dont want to build on every index...')
                         return
                     }
