289356ffe1bb3f0e01c0c72923142db28f753fd0
jasonluck <jluck@meetveracity.com>
2017-10-16 11:09:12 -0400

    use the mavenBuild dsl

--- vars/microservicePipeline.groovy
+++ vars/microservicePipeline.groovy
@@ -24,58 +24,8 @@ def call(body) {
 
             dir("${config.directory}") {
 
-                stage('Maven Build') {
-                    def mavenSettings = libraryResource 'com/lucksolutions/maven/settings.xml'
-                    writeFile file: 'settings.xml', text: mavenSettings
-                    withCredentials([usernamePassword(credentialsId: 'nexus', usernameVariable: 'DEPLOY_USER', passwordVariable: 'DEPLOY_PASSWORD')]) {
-                        sh 'mvn -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Ddockerfile.skip=true -s settings.xml clean compile test-compile'
-                    }
-                }
-
-                try {
-                    stage('Unit Testing') {
-                        sh 'mvn -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Ddockerfile.skip=true -Dmaven.test.failure.ignore=true -s settings.xml test'   
-                    }
-                } finally {
-                    step([$class: 'JUnitResultArchiver', testResults: '**/surefire-reports/*.xml', healthScaleFactor: 1.0, allowEmptyResults: true])
-                    if (currentBuild.result == 'UNSTABLE') {
-                        return 
-                    }
-                }
-                stage('Package') {
-                    try {
-                        sh 'mvn -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Ddockerfile.skip=true -s settings.xml package'
-                    } finally {
-                        publishHTML (target: [
-                            allowMissing: true,
-                            alwaysLinkToLastBuild: false,
-                            keepAll: true,
-                            reportDir: 'target/site/jacoco',
-                            reportFiles: 'index.html',
-                            reportName: "Code Coverage"
-                        ])
-                    }
-                }
-
-                stage('Code Analysis') {
-                    withSonarQubeEnv('CI') {
-                        sh 'mvn -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Ddockerfile.skip=true -s settings.xml sonar:sonar'
-                    }
-                }
-
-                stage("Quality Gate") {
-                    timeout(time: 15, unit: 'MINUTES') {
-                        def qg = waitForQualityGate()
-                        if (qg.status != 'OK') {
-                            error "Pipeline aborted due to quality gate failure: ${qg.status}"
-                        }
-                    }
-                }
-
-                stage('Deploy to Repository') {
-                    withCredentials([usernamePassword(credentialsId: 'nexus', usernameVariable: 'DEPLOY_USER', passwordVariable: 'DEPLOY_PASSWORD')]) {
-                        sh 'mvn -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Ddockerfile.skip=true -s settings.xml deploy'
-                    }
+                mavenBuild {
+                    directory = config.directory
                 }
 
                 dockerBuild {
