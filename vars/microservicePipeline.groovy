ae4c19d6f9545fdd8fb4d6b67431bd6eafd175d4
jasonluck <jluck@meetveracity.com>
2017-10-16 14:10:32 -0400

    try parallel builds

--- vars/microservicePipeline.groovy
+++ vars/microservicePipeline.groovy
@@ -9,6 +9,12 @@ def call(body) {
         config.directory = '.'
     }
 
+    if (config.dockerBuilds == null) {
+        config.dockerBuilds = [
+                config.imageName: config.directory
+        ]
+    }
+
     node {
         properties([
             pipelineTriggers([
@@ -28,10 +34,17 @@ def call(body) {
                     directory = config.directory
                 }
 
-                dockerBuild {
-                    directory = config.directory
-                    imageName = config.imageName
+                def keys = config.dockerBuilds.keySet();
+                def builds = [:]
+                for (int i=0; i<keys.size(); ++i) {
+                    builds["echo ${keys[i]}": dockerBuild {
+                        directory = config.dockerBuilds[keys[i]]
+                        imageName = keys[i]
+                    }]
                 }
+                sh "echo ${builds}"
+
+                parallel builds
             }
         } finally {
             //Send build notifications if needed
