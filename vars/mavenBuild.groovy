7ce3ef30532afb68d54f22f208eb1f39e8d24bff
jasonluck <jluck@meetveracity.com>
2017-10-19 12:49:16 -0400

    fix credential binding class

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -66,7 +66,7 @@ def call(body) {
             withSonarQubeEnv('CI') {
                 if (isPullRequest()) {
                     //Use Preview mode for PRs
-                    withCredentials(string(credentialsId: 'Github', variable: 'GITHUB_TOKEN')) {
+                    withCredentials(StringBinding(credentialsId: 'Github', variable: 'GITHUB_TOKEN')) {
                         sh "${mvnCmd} -Dsonar.analysis.mode=preview -Dsonar.github.pullRequest=${env.CHANGE_ID} -Dsonar.github.oauth=${GITHUB_TOKEN} sonar:sonar"
                     }
                 } else {
