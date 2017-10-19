27e4f3ae7bb2ac56bd0d496cdd2770e1e6d5ca75
jasonluck <jluck@meetveracity.com>
2017-10-19 14:48:21 -0400

    remove indexing code

--- vars/ignoreIndexing.groovy
+++ vars/ignoreIndexing.groovy
@@ -1,17 +1,17 @@
 //Skip the build if this was caused by branch indexing and its not the first build
 def call() {
 
-    //Check to see if its the first build
-    if (currentBuild.getPreviousBuild() != null) {
+    // //Check to see if its the first build
+    // if (currentBuild.getPreviousBuild() != null) {
         
-        def causes = currentBuild.rawBuild.getCauses()
-        for (int i=0; i<causes.size(); ++i) {
-            if (causes[i].getShortDescription().contains('Branch indexing')) {
-                //Don't build since this was just branch indexing
-                currentBuild.result = 'SUCCESSFUL'
-                echo('Stopping since we dont want to build on every index...')
-                return
-            }
-        }
-    }
+    //     def causes = currentBuild.rawBuild.getCauses()
+    //     for (int i=0; i<causes.size(); ++i) {
+    //         if (causes[i].getShortDescription().contains('Branch indexing')) {
+    //             //Don't build since this was just branch indexing
+    //             currentBuild.result = 'SUCCESSFUL'
+    //             echo('Stopping since we dont want to build on every index...')
+    //             return
+    //         }
+    //     }
+    // }
 }
