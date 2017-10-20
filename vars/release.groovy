85ff06e1a61bc4d4d1fc9680ffbf115ee623a750
jasonluck <jluck@meetveracity.com>
2017-10-20 13:51:38 -0400

    More release pipeline

--- vars/release.groovy
+++ vars/release.groovy
@@ -52,28 +52,45 @@ def call(body) {
                 }
             }
 
+            def tmpDir = pwd(tmp: true)
+            def mvnCmd = "mvn -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Ddockerfile.skip=true -DskipITs=true -s ${tmpDir}/settings.xml"
+
+            stage('Configure Maven') {
+                def mavenSettings = libraryResource 'com/lucksolutions/maven/settings.xml'
+                writeFile file: "${tmpDir}/settings.xml", text: mavenSettings
+            }
+
             stage('Set Release Version') {
                 //Set release version
-                //Check for any snapshot versions remaining
+                sh "${mvnCmd} version:set -DnewVersion=${versions['release']}"
 
-                //Commit changes locally
+                //Update SNAPSHOT dependencies to their release versions if available
+                sh "${mvnCmd} versions:use-releases"
+
+                //Check for any snapshot versions remaining
+                //sh "${mvnCmd} enforcer:enforce -Denforcer.fail=true"
             }
 
             stage('Tag Release') {
                 //Tag release
                 sh "git tag -a ${versions['release']} -m \"Release version ${versions['release']}\""
                 //Commit changes locally
+                sh "git commit -a -m \"Releasing version ${versions['release']}\""
             }
 
-            stage('Run Build Steps') {
-                //Run the build steps for this project type
+            //Build the new release
+            mavenBuild {
+                directory = config.directory
             }
 
             stage('Set Next Development Version') {
                 //Set the next dev version
+                sh "${mvnCmd} version:set -DnewVersion=${versions['development']}"
                 //Commit changes locally
+                sh "git commit -a -m \"Preparing POMs for next development version ${versions['development']}\""
 
                 //Push to Github
+                sh "git status"
             }
         }
     }
